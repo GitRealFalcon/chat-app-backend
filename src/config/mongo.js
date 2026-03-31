9f6b91ae5ce2d699a9c265f7724fc0dc3abc1b0f
Mohd Naeem <monaeem8@gmail.com>
2026-03-31 18:22:39 +0530

    fix: improve MongoDB connection URI handling

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -3,7 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const DBConnect = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`)
+        const mongoURI = process.env.MONGO_URI || process.env.MONGO_URL
+        const connectionInstance = await mongoose.connect(`${mongoURI}${DB_NAME}`)
         console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MongoDB Connection Error:", error);
